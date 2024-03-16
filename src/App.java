e382bb7945437741076e2004c93e4026e17c6c4d
Victor Pinheiro Dev <reivictor.dev@gmail.com>
2024-03-15 23:48:02 -0300

    opt 0 and 1

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -17,21 +18,44 @@ public class App {
         int itemToPush2 = stack2.pop();
 
         //
-        System.out.println(stack1.printStack());
-        System.out.println(stack2.printStack());
+        
 
         System.out.println(itemToPush);
         emptyStack.push(itemToPush);
-        System.out.println(emptyStack.printStack());
+        
 
         // Organizar a estrura dentro do while com as opções
 
+        
         while (true) {
+    
             System.out.println("---------BEM VINDO---------");
             System.out.println(
-                    "---------Ao final do jogo, a metade dos números menores deverá ficar na pilha 1.---------");
-            System.out.println("---------A metade dos números maiores deverá ficar na pilha 2.---------");
-            System.out.println("---------A pilha 3 será utilizada para as manobras.---------");
+                    "---------Ao final do jogo, a metade dos números menores deverá ficar na pilha 1.");
+            System.out.println("---------A metade dos números maiores deverá ficar na pilha 2.");
+            System.out.println("---------A pilha 3 será utilizada para as manobras.");
+            System.out.println("________________________________________________________________________");
+            System.out.println("---------1# Verificar as pilhas");
+            System.out.println("---------2# Retirar de uma pilha");
+            System.out.println("---------3# Utilizar a pilha de transição");
+            System.out.println("---------4# Retirar da pilha de transição e colocar em outra");
+            System.out.println("---------0# Encerrar");
+
+            Scanner scn = new Scanner(System.in);
+            int option = scn.nextInt();
+
+            if (option == 0) {
+                break;
+            }
+
+            if(option == 1) {
+                System.out.println("Pilha 1(menores): "+stack1.printStack());
+                System.out.println("**");
+                System.out.println("Pilha de transição: "+emptyStack.printStack());
+                System.out.println("**");
+                System.out.println("Pilha 2(maiores): "+stack2.printStack());
+                continue;
+            }
         }
     }
 }
